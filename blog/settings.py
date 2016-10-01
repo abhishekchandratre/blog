de6a79ba59c8fb5b990ae5afe901744acf48b3f5
Abhishek Chandratre <abhishek.chandratre@gmail.com>
2016-10-01 21:16:59 +0000

    Changed setting.py to work with django server, now static files are collected on /static folder

--- blog/settings.py
+++ blog/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Use static dir if you use static files from other directory.
 # STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"), ]
-# Please done use static root as of now 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
